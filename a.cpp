29e72467f0ec335f5048989225403fd20801fe74
i314q159 <i314q159@outlook.com>
2026-02-03 15:38:29 +0800

    start cpp learning

---
+++ a.cpp
@@ -0,0 +1,20 @@
+#include "a.h"
+
+#include <iostream>
+
+void a() {
+    auto x = 1;
+
+    auto add1 = [](auto a, auto b) -> auto {
+        return a + b - a;
+    };
+
+    for (auto i = 'a'; i <= 'z'; i++) {
+        auto sum1 = add1(x, i);
+
+        // std::string(1, i) 是创建一个包含1个字符 i 的字符串
+        // std::to_string 是 C++11 引入的标准库函数，用于将数值类型转换为字符串。
+        auto str1 = std::string(1, i) + ":" + std::to_string(sum1);
+        std::cout << str1 << std::endl;
+    }
+}
---
+++ a.h
@@ -0,0 +1,6 @@
+#ifndef CPP_LEARN_A_H
+#define CPP_LEARN_A_H
+
+
+void a();
+#endif //CPP_LEARN_A_H
