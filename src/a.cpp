9b30b5502bba6e88321d211c5f93579fd6a31e07
i314q159 <i314q159@outlook.com>
2026-02-05 19:32:47 +0800

    Update a.cpp

--- src/a.cpp
+++ src/a.cpp
@@ -12,13 +12,11 @@ void Test::a() {
         auto x = 1;
         auto sum1 = add1(x, i);
 
-        // std::string(1, i) 是创建一个包含1个字符 i 的字符串
-        // std::to_string 是 C++11 引入的标准库函数，用于将数值类型转换为字符串。
         map_A[std::string(1, i)] = sum1;
     }
 
-    for (auto &p: map_A) {
-        std::cout << p.first << ":" << p.second << std::endl;
+    for (const auto& [x, y] : map_A) {
+        std::cout << x << ":" << y << std::endl;
     }
 
     b();
