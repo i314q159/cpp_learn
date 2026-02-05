6a8f3ce077eb699d84ce81b0a0be9c684c5e5e9a
i314q159 <i314q159@outlook.com>
2026-02-05 15:16:18 +0800

    Update a.cpp

--- src/a.cpp
+++ src/a.cpp
@@ -1,11 +1,12 @@
 #include "a.h"
 
 #include <iostream>
+#include <map>
 
 void Test::a() {
-    auto add1 = [](auto a, auto b) -> auto {
-        return a + b - a;
-    };
+    auto add1 = [](auto a, auto b) -> auto { return a + b - a; };
+
+    std::map<std::string, int> map_A;
 
     for (auto i = 'a'; i <= 'z'; i++) {
         auto x = 1;
@@ -13,9 +14,13 @@ void Test::a() {
 
         // std::string(1, i) 是创建一个包含1个字符 i 的字符串
         // std::to_string 是 C++11 引入的标准库函数，用于将数值类型转换为字符串。
-        auto str1 = std::string(1, i) + ":" + std::to_string(sum1);
-        std::cout << str1 << std::endl;
+        map_A[std::string(1, i)] = sum1;
     }
+
+    for (auto &p: map_A) {
+        std::cout << p.first << ":" << p.second << std::endl;
+    }
+
     b();
 }
 
